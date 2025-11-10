66c4c7838757ff0ab95080349a9a4e6dfde6c65a
Nguyen Dinh Phong <dinhphong2222004@gmail.com>
2025-11-10 21:00:41 +0700

    haa

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,57 @@
 
 
 
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite' // ← THÊM DÒNG NÀY
+// import path from 'path'
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss() // ← THÊM DÒNG NÀY
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+//   server: {
+//     port: 5173,
+//   },
+//   build: {
+//     outDir: 'dist',
+//     sourcemap: false,
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // ← THÊM DÒNG NÀY
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss() // ← THÊM DÒNG NÀY
+    tailwindcss()
   ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  server: {
-    port: 5173,
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react/jsx-runtime']
   },
   build: {
     outDir: 'dist',
     sourcemap: false,
-  }
+    commonjsOptions: {
+      include: [/node_modules/],
+      transformMixedEsModules: true
+    }
+  },
+  server: {
+    port: 5173,
+  },
 })
